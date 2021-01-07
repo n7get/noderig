9543568d0eaf38c45d27ec2bd09a9ef6406d2636
Robert Ambrose <rna@muttsoft.com>
2021-01-07 12:19:49 -0800

    Cleaned up alc-status.

--- uibuilder/noderig/src/alc-status.vue
+++ uibuilder/noderig/src/alc-status.vue
@@ -1,6 +1,5 @@
-
 <template>
-    <div :class="{'alc-status': true, on: on}" @click="sendClick">
+    <div :class="{'alc-status': true, on: on}">
         ALC
     </div>
 </template>
@@ -12,24 +11,12 @@ module.exports = {
             on: false
         }
     },
-    methods: {
-        sendClick: e => {
-        uibuilder.send({topic: 'LOCK_EVENT', payload: 'click'});
-        },
-    },
     mounted() {
         const self = this;
 
         uibuilder.onChange('msg', msg =>{
             if(msg.hasOwnProperty('alc_status')) {
-                console.log('msg: ' + JSON.stringify(msg));
-
-                if(msg.alc_status) {
-                    self.on = true;
-                }
-                else {
-                    self.on = false;
-                }
+                self.on = msg.alc_status;
             }
         });
     }
