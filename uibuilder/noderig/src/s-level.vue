b09b9a23d04407acb7348ac9dcbbe5b78ba39b7e
n7get <gethub@muttsoft.com>
2021-02-09 09:33:55 -0800

    Added generic level component.

---
+++ uibuilder/noderig/src/s-level.vue
@@ -0,0 +1,68 @@
+<template>
+    <b-row>
+        <div class="col-4">
+            {{ label }}<span v-if="show !== 'none'"> ({{ value }})</span>
+        </div>
+        <b-form-input
+	    class="col-7"
+            v-model="value"
+            type="range"
+            :min="min"
+            :max="max"
+            @change="sendChange"
+        ></b-form-input>
+    </b-row>
+</template>
+
+<script>
+module.exports = {
+    props: {
+        event: {
+            type: String,
+            required: true,
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+        min: {
+            type: Number,
+            default: 0,
+        },
+        max: {
+            type: Number,
+            max: 100,
+        },
+        show: {
+            type: String,
+            default: '',
+        },
+    },
+    data: function() {
+        return {
+            value: undefined,
+        }
+    },
+    methods: {
+        sendChange: function(new_value) {
+            uibuilder.send({topic: this.event, event: 'set', value: new_value});
+        },
+    },
+    mounted: function() {
+        var self = this;
+console.log('show: ' + this.show);
+        uibuilder.onChange('msg', msg => {
+            var p = msg.payload;
+
+            if(p.name === this.event) {
+                if(p.hasOwnProperty('value')) {
+                    self.value = p.value;
+                }
+            }
+        });
+    }
+}
+</script>
+
+<style scoped>
+</style>
